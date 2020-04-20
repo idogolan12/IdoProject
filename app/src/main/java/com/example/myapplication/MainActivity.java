f8dac182b5c0a129b834c69021107f5ddf40e07c
idogolan12 <idogolan12312@gmail.com>
2020-04-20 15:04:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    int aa = 0;
+    Intent go;
+    String av;
+    GridLayout BGT;
+    LinearLayout LLLet;
+    int btnWidth = 100, btnHeight = 100;
+    String a = "כיטחזוהדגבאתשרקצפעסנמלץףךןם";
+    int G = a.length();
+    Button[] bt = new Button[G];
+    TextView[] tv;
+    ArrayList<String> words = new ArrayList<>();
+    int[] IVS = {R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g};
+    ImageView IV;
+    InputStream is;
+    InputStreamReader isr;
+    BufferedReader br;
+    String st;
+    int size;
+    int counter = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BGT = findViewById(R.id.BGT);
+        IV = findViewById(R.id.iv);
+        go = new Intent(this,MainActivity.class);
+        BGT.setColumnCount(11);
+        Build_board2();
+        newGame();
+
+
+    }
+
+    private void newGame() {
+        counter = 0;
+        LLLet = findViewById(R.id.LLLet);
+        is = getResources().openRawResource(R.raw.animals);
+        isr = new InputStreamReader(is);
+        br = new BufferedReader(isr);
+        LLLet.removeAllViews();
+        while (true) {
+            try {
+                if (!((st = br.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            words.add(st);
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        size = words.size();
+        int num = (int) ((size * Math.random()));
+        tv = new TextView[words.get(num).length()];
+        Build_board1(words.get(num).length() , words.get(num));
+        av = words.get(num);
+    }
+
+
+    private void Build_board1(int NumLet , String st1) {
+        //LLLet.clear...
+        for (int i = 0; i < NumLet; i++) {
+            tv[i] = new TextView(LLLet.getContext());
+            LLLet.addView(tv[i], btnWidth, btnHeight);
+            tv[i].setText("____");
+            if (st1.charAt(i) == ' ') {
+                tv[i].setVisibility(View.INVISIBLE);
+                counter=1;
+            }
+        }
+    }
+
+    private void Build_board2() {
+        //BGT.clear...
+        for (int i = 0; i < bt.length; i++) {
+            bt[i] = new Button(BGT.getContext());
+            BGT.addView(bt[i], btnWidth, btnHeight);
+            String ab = a.charAt(i) + "";
+            bt[i].setText(ab);
+            bt[i].setOnClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String a = ((Button) v).getText().toString();
+        if (av.indexOf(((Button) v).getText().toString()) != -1)
+        {
+            for (int i = 0; i < av.length(); i++)
+            {
+                if (((Button) v).getText().equals(av.charAt(i) + ""))
+                {
+                    tv[av.length() - i - 1].setText(((Button) v).getText());
+                    counter++;
+                    if (counter == av.length())
+                        newGame();
+
+
+                }
+
+
+            }
+        }
+        else
+        {
+            if(aa < 5)
+            {
+                IV.setImageResource(IVS[aa]);
+                aa++;
+                Toast.makeText(this, "dosen't exist", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                IV.setImageResource(IVS[aa]);
+                finishGame();
+            }
+
+
+
+
+
+        }
+    }
+
+    private void finishGame()
+    {
+        Toast.makeText(this, "finish game", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("איש תלוי                                                ");
+        adb.setMessage("הפסדת! אתה רוצה לשחק שוב באותו נושא?לשחק בנושאים אחרים של המשחק איש תלוי או לשחק במשחק אחר");
+        adb.setPositiveButton("לשחק שוב באותו נושא", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(go);
+            }
+        });
+        adb.setNegativeButton("לבחור בנושאים אחרים במשחק איש תלוי", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "aa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        adb.setNeutralButton("לשחק במשחק אחר", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "work", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+}
